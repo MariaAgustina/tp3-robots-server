401f6d716fa0f8e3f06821f87c7612f95197c125
Maria Agustina Markosich <agustina@noblypos.com>
2021-08-29 22:26:44 -0300

    added similarities

--- upload-image.py
+++ upload-image.py
@@ -1,31 +1,47 @@
 import falcon, json
 import base64
-import os
+import os, shutil
 
 class ProcessImageResource(object):
+  def __init__(self):
+    print("Server initialized")
+
+  def removeContents(self,folder):
+    for filename in os.listdir(folder):
+      file_path = os.path.join(folder, filename)
+      try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+          os.unlink(file_path)
+        elif os.path.isdir(file_path):
+          shutil.rmtree(file_path)
+      except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+
   def on_post(self, req, resp):
+    self.removeContents("results/")
+    self.removeContents("uploaded-images/")
+
     input_image_json = req.media
-    image = input_image_json["image"]
-    name = input_image_json["name"]
-    if(name):
-      image_dir = "uploaded-images/{}".format(name)
+    for image_and_id_dictionaries in input_image_json:
+      image = image_and_id_dictionaries["image"]
+      name = image_and_id_dictionaries["id"]
 
-      decodeit = open(image_dir, 'wb')
-      decodeit.write(base64.b64decode((image)))
-      decodeit.close()
-      os.system('python yolov5/detect.py --weights weights-yolo.pt --source {} --project results/ --save-txt --save-crop --save-conf --augment'.format(image_dir))
+      if(name):
+        image_dir = "uploaded-images/{}.png".format(name)
 
-      os.chdir('results/exp/crops') #TODO: muchos exp
-      all_subdirs = [d for d in os.listdir('.') if os.path.isdir(d)] #TODO: podriamos pasarlos con la confianza para cuando hay mas de uno y asi el cliente muestra el de mayor confianza o hacerlo aca directamente, aparte si no hay subdirs ver que pasa
-      self.result = {"result": all_subdirs}
-      resp.body = json.dumps(self.result)
+        decodeit = open(image_dir, 'wb')
+        decodeit.write(base64.b64decode((image)))
+        decodeit.close()
 
-    else:
-      #TODO: reponder 500
-      self.result = []
-      resp.body = json.dumps(self.result)
+    os.system('python get_image_feature_vectors.py')
+    os.system('python cluster_image_feature_vectors.py')
+
+    f = open('nearest_neighbors.json','rb')
+    data = json.load(f)
+    resp.body = json.dumps(data)
+    f.close()
 
 
-api = falcon.API()
+app = application = falcon.App()
 companies_endpoint = ProcessImageResource()
-api.add_route('/upload-image', companies_endpoint)
+app.add_route('/upload-image', companies_endpoint)
